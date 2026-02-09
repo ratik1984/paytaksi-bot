6744d9989fda322864b3f8752c9986f4617a1b9a
ratik1984 <haciratik@gmail.com>
2026-02-09 21:28:02 +0400

    Delete src directory

--- src/pricing.js
+++
@@ -1,4 +0,0 @@
-export function calcPrice({ baseFare, perKm, perMin }, { distanceKm = 0, durationMin = 0 }) {
-  const v = baseFare + perKm * distanceKm + perMin * durationMin
-  return Math.round(v * 100) / 100
-}
