ce8b2c40eccc6172637c0c68386e6c2766ed62dd
ratik1984 <haciratik@gmail.com>
2026-02-11 23:53:09 +0400

    config.js

--- miniapp-driver/config.js
+++ miniapp-driver/config.js
@@ -1,1 +1,5 @@
-window.APP_CONFIG={BACKEND_BASE_URL:"http://localhost:8080",API_KEY:"change_me_long_random",MAPBOX_TOKEN:""};
+window.APP_CONFIG={
+  BACKEND_BASE_URL:"https://paytaksi-bot-1.onrender.com",
+  API_KEY:"guclu_random_key_12345",
+  MAPBOX_TOKEN:"SENIN_MAPBOX_TOKEN"
+};
